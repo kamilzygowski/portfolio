954d8f53b26d2b125954736e011e014520dc367f
swedishsailor <kzygowski7@gmail.com>
2022-03-08 23:07:27 +0100

    Swiper configuration

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,11 +1,37 @@
 import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Mousewheel } from 'swiper';
 import Home from '../Home/Home'
+import Projects from '../Projects/Projects'
 import './App.scss'
+import 'swiper/css';
 
 const App = () => {
   return (
     <div className='App'>
-      <Home />
+      <Swiper
+        direction={"horizontal"}
+        slidesPerView={1}
+        spaceBetween={30}
+        mousewheel={true}
+        pagination={{
+          clickable: true,
+          type: "progressbar",
+        }}
+        modules={[Mousewheel, Pagination]}
+        observeParents={true}
+        observer={true}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+        <Home />
+        </SwiperSlide>
+        <SwiperSlide>
+        <Projects />
+        </SwiperSlide>
+      </Swiper>
+      
+      
     </div>
   )
 }
---
+++ src/Projects/Projects.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Projects.scss'
+
+const Projects = () => {
+    return (
+        <div className='Projects'>
+            Projects
+        </div>
+    )
+}
+
+export default Projects
