6f2d32a8b167bad52371accc5e923fade934bd69
Kamil Żygowski <kzygowski7@gmail.com>
2022-04-15 18:29:23 +0200

    PopUp component basic layout

--- src/Games/Games.tsx
+++ src/Games/Games.tsx
@@ -10,12 +10,16 @@ interface ListStyle {
 }
 
 const Games = () => {
+  const gamesInfoArr = [{title:'Dimension Eye', description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  img:'https://i.postimg.cc/vHrrQTby/deprev.png', href:'https://swedishsailor.github.io/dimensioneye/'}]
   const allElems = document.querySelectorAll(".gamesLi")
   const liStyles: ListStyle[] = [{ z: 40, right: 600, width: 600, bottom: 0 }, { z: 35, right: 450, width: 600, bottom: 65 },
   { z: 30, right: 300, width: 600, bottom: 130 }, { z: 25, right: 150, width: 600, bottom: 195 }, { z: 20, right: 0, width: 600, bottom: 260 }];
   // Controll the state of being hovered (or not)
   const [isHovered, setHover] = useState(false);
   const [isClicked, setClick] = useState(false);
+  const [gameInfo, setGameInfo] = useState({title:'Dimension Eye', description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+   img:'https://i.postimg.cc/vHrrQTby/deprev.png', href:'https://swedishsailor.github.io/dimensioneye/'});
 
   useEffect(() => {
     allElems.forEach((element: any, index: number) => {
@@ -50,7 +54,7 @@ const Games = () => {
 
   return (
     <div className='Games'>
-            {isClicked ? <PopUp></PopUp> : null}
+            {isClicked ? <PopUp setClick={setClick} gameInfo={gameInfo}></PopUp> : null}
       <ul className='gamesUl'>
         {/* TITLE LINK*/}
         <li className='gamesLi' id="1">
@@ -58,19 +62,19 @@ const Games = () => {
         </li>
         {/* DIMENSION EYE */}
         <li className='gamesLi' id="2">
-          <div id="2" onMouseEnter={(e) => handleMouseEnter(e)} onMouseLeave={(e) => handleMouseLeave(e)} className='gameInfo'><h3>Dimension Eye</h3></div>
+          <div id="2" onMouseEnter={(e) => handleMouseEnter(e)} onMouseLeave={(e) => handleMouseLeave(e)} className='gameInfo' onClick={() => setClick(true)}><h3>Dimension Eye</h3></div>
         </li>
         {/* MOUSE SABRE */}
         <li className='gamesLi' id="3">
-          <div id="3" onMouseEnter={(e) => handleMouseEnter(e)} onMouseLeave={(e) => handleMouseLeave(e)} className='gameInfo'><h3>Mouse sabre</h3></div>
+          <div id="3" onMouseEnter={(e) => handleMouseEnter(e)} onMouseLeave={(e) => handleMouseLeave(e)} className='gameInfo' onClick={() => setClick(true)}><h3>Mouse sabre</h3></div>
         </li>
         {/*MILION LASERS */}
         <li className='gamesLi' id="4">
-          <div id="4" onMouseEnter={(e) => handleMouseEnter(e)} onMouseLeave={(e) => handleMouseLeave(e)} className='gameInfo'><h3>milion lasers</h3></div>
+          <div id="4" onMouseEnter={(e) => handleMouseEnter(e)} onMouseLeave={(e) => handleMouseLeave(e)} className='gameInfo' onClick={() => setClick(true)}><h3>milion lasers</h3></div>
         </li>
         {/* IT'S GONNA BE OK. */}
         <li className='gamesLi' id="5">
-          <div id="5" onMouseEnter={(e) => handleMouseEnter(e)} onMouseLeave={(e) => handleMouseLeave(e)} className='gameInfo'><h3>it's gonna be ok</h3></div>
+          <div id="5" onMouseEnter={(e) => handleMouseEnter(e)} onMouseLeave={(e) => handleMouseLeave(e)} className='gameInfo' onClick={() => setClick(true)}><h3>it's gonna be ok</h3></div>
         </li>
       </ul>
     </div>
--- src/Games/PopUp/PopUp.tsx
+++ src/Games/PopUp/PopUp.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import './PopUp.scss'
 
-const PopUp = () => {
+const PopUp = (props: any) => {
   return (
-    <div className='PopUp'>
-        <div className='window'>
-            XDD
+    <div className='PopUp' onClick={() => props.setClick(false)}>
+      <div className='window'>
+        <div className='info'>
+          <div className='text'>
+            <h2> {props.gameInfo.title} </h2>
+            <p className='description'>
+              {props.gameInfo.description}
+            </p>
+            <a className='gameLink' href={props.gameInfo.href}> Check out </a>
+          </div>
+          <img src={props.gameInfo.img} alt="game preview" className='descriptionImg'></img>
         </div>
+      </div>
     </div>
   )
 }
