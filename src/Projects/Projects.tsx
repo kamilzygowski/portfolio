ca2ca582dba1d44924c1cfc0e87d9fafb0d223ea
Kamil Zygowski <kzygowski7@gmail.com>
2022-04-23 16:10:06 +0200

    preparing Project component for draggable windows

--- src/Projects/Projects.tsx
+++ src/Projects/Projects.tsx
@@ -7,6 +7,17 @@ interface Project {
 
 const Projects = () => {
     const [project, setProjects] = useState<Project[]>([{ src: 'essa' }]);
+    const handleWindowMove = (e:any) => {
+        const windowsWindow:any = document.querySelector('.window');
+        console.log(e)
+        /*const mouseX = e.clientX;
+        const mouseY = e.clientY;
+
+        console.log(e.target.style)
+        windowsWindow.style.position = 'absolute';
+        windowsWindow.style.left = `${mouseX}px`;
+        windowsWindow.style.zIndex = 55;*/
+    }
     const handleClose = () => {
 
     }
@@ -20,13 +31,13 @@ const Projects = () => {
         <div className='Projects'>
             {/*<iframe src="https://swedishsailor.github.io/portfolio-game/" className='iproject' title='portfolio game' />*/}
             <h3>Projects</h3>
-            <div className='interactivePortfolio'>
+            {/*<div className='interactivePortfolio'>
                 <img src="https://i.postimg.cc/7Z3Lrxpt/sassss.png" alt="Interactive Portfolio" />
                 <p>Checkout my interactive portfolio</p>
-            </div>
+    </div>*/}
             <div className='projectsList'>
-                <div className='window'>
-                    <div className='frame'><div className='topBar'><div className='Btn closeBtn'></div><div className='Btn maxBtn'></div><div className='Btn minBtn'></div></div></div>
+                <div className='window' draggable={true}>
+                    <div className='frame'><div className='topBar' draggable={true} onDragStart={(e) => handleWindowMove(e)}><div className='Btn closeBtn'></div><div className='Btn maxBtn'></div><div className='Btn minBtn'></div></div></div>
                     <img src="https://i.postimg.cc/MpCrVwHw/planetraium.png" alt="project" />
                 </div>
                 <div className='window'>
