b2f1dfd79351906e4917bfb2da0117fc2ca52392
Kamil Żygowski <kzygowski7@gmail.com>
2022-05-05 04:01:19 +0200

    Projects: map windows & minimized, handle onclicks

--- src/Projects/Projects.tsx
+++ src/Projects/Projects.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import './Projects.scss'
 
 interface Project {
@@ -6,9 +6,12 @@ interface Project {
 }
 
 const Projects = () => {
-    const [project, setProjects] = useState<Project[]>([{ src: 'essa' }]);
-    const handleWindowMove = (e:any) => {
-        const windowsWindow:any = document.querySelector('.window');
+    const windowElement: any = useRef(null);
+    const minimizedWindowsElement: any = useRef(null)
+    const [projects, setProjects] = useState<Project[]>([{ src: "https://i.postimg.cc/MpCrVwHw/planetraium.png" }, { src: "https://i.postimg.cc/zvCBj8Sg/franky.png" }]);
+    const [minimizedWindow, setMinimizedWindows] = useState<Project[]>([])
+    const handleWindowMove = (e: any) => {
+        const windowsWindow: any = document.querySelector('.window');
         console.log(e)
         /*const mouseX = e.clientX;
         const mouseY = e.clientY;
@@ -18,10 +21,42 @@ const Projects = () => {
         windowsWindow.style.left = `${mouseX}px`;
         windowsWindow.style.zIndex = 55;*/
     }
-    const handleClose = () => {
+    const handleClose = (e: any) => {
+        // Fix the array to remove this window
+        setProjects([
+            ...projects.slice(0, e.target.id),
+            ...projects.slice(e.target.id + 1)
+        ])
+    }
+    const handleUnMinimize = (e: any) => {
+
+        // Then display the window again on the main screen
+        setProjects([
+            ...projects,
+            ...[minimizedWindow[e.target.id]]
+        ])
+        // First delete the minimized icon 
+        setMinimizedWindows([
+            ...minimizedWindow.slice(0, e.target.id),
+            ...minimizedWindow.slice(e.target.id + 1)
+        ])
+
 
     }
-    const handleMinimize = () => {
+    const handleMinimize = (e: any) => {
+
+        handleClose(e);
+        setMinimizedWindows([
+            ...[projects[e.target.id]],
+            ...minimizedWindow
+        ])
+        // windowElement.current.style = "position: fixed; bottom:0; transform:scale(0.35); transition:0.7s; display:none"
+        //const mirror = windowElement.cloneNode(false);
+        //console.log(mirror)
+        //mirror.style += 'background:url("https://i.postimg.cc/rp57qS8t/981-9817075-vector-free-what-is-microsoft-explorer-internet-explorer.png")'
+        //console.log(windowElement.current)
+        //minWindow.innerHTML = '<img src="https://i.postimg.cc/rp57qS8t/981-9817075-vector-free-what-is-microsoft-explorer-internet-explorer.png"/>'
+        //minimizedWindowsElement.appendChild(minWindow)
 
     }
     const handleMaximize = () => {
@@ -35,17 +70,47 @@ const Projects = () => {
                 <img src="https://i.postimg.cc/7Z3Lrxpt/sassss.png" alt="Interactive Portfolio" />
                 <p>Checkout my interactive portfolio</p>
     </div>*/}
-            <div className='projectsList'>
-                <div className='window' draggable={true}>
-                    <div className='frame'><div className='topBar' draggable={true} onDragStart={(e) => handleWindowMove(e)}><div className='Btn closeBtn'></div><div className='Btn maxBtn'></div><div className='Btn minBtn'></div></div></div>
+            <div className='projectsList' ref={windowElement}>
+                {projects.map((element: any, index: number) => {
+                    return (
+                        <div className='window' draggable={true} id={index.toString()} key={index}>
+                            <div className='frame'>
+                                <div className='topBar' draggable={true} onDragStart={(e) => handleWindowMove(e)}>
+                                    <div className='Btn closeBtn' id={index.toString()} onClick={(e) => handleClose(e)}>
+                                    </div>
+                                    <div className='Btn maxBtn'>
+                                    </div>
+                                    <div onClick={(e) => handleMinimize(e)} className='Btn minBtn' id={index.toString()}>
+                                    </div>
+                                </div>
+                            </div>
+                            <img src={element.src} alt="project" />
+                        </div>
+                    )
+                })}
+                {/*} <div className='window' draggable={true} id="0">
+                    <div className='frame'><div className='topBar' draggable={true} onDragStart={(e) => handleWindowMove(e)}><div className='Btn closeBtn' id="0" onClick={(e)=> handleClose(e)}></div><div className='Btn maxBtn'></div><div onClick={(e) => handleMinimize(e)} className='Btn minBtn'></div></div></div>
+                    <img src="https://i.postimg.cc/MpCrVwHw/planetraium.png" alt="project" />
+                </div>
+                <div className='window' draggable={true} id="1">
+                    <div className='frame'><div className='topBar' draggable={true} onDragStart={(e) => handleWindowMove(e)}><div className='Btn closeBtn'></div><div className='Btn maxBtn'></div><div onClick={(e) => handleMinimize(e)} className='Btn minBtn'></div></div></div>
                     <img src="https://i.postimg.cc/MpCrVwHw/planetraium.png" alt="project" />
                 </div>
                 <div className='window'>
                     <div className='frame'><div className='topBar'><div className='Btn closeBtn'></div><div className='Btn maxBtn'></div><div className='Btn minBtn'></div></div></div>
                     <img src="https://i.postimg.cc/zvCBj8Sg/franky.png" alt="project" />
-                </div>
+                </div> */}
             </div>
+            <ul className='minimizedWindows' ref={minimizedWindowsElement}>
+                {minimizedWindow.map((element: any, index: number) => {
+                    return (
+                        <li className='minimizedWindow' onClick={(e) => handleUnMinimize(e)} id={index.toString()} key={index}>
+                            <img src="https://i.postimg.cc/rp57qS8t/981-9817075-vector-free-what-is-microsoft-explorer-internet-explorer.png" />
+                        </li>
+                    )
+                })}
 
+            </ul>
 
         </div>
     )
