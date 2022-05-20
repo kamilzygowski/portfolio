6b761f2b3ed4ad5a91f0bdfd199516d50868e2cd
Kamil Żygowski <kzygowski7@gmail.com>
2022-05-20 22:49:33 +0200

    Projects: minimize & unminimize work well

--- src/Projects/Projects.tsx
+++ src/Projects/Projects.tsx
@@ -3,12 +3,18 @@ import './Projects.scss'
 
 interface Project {
     src: string;
+    id: number;
 }
 
 const Projects = () => {
+    const initialProjects = [
+        { src: "https://i.postimg.cc/MpCrVwHw/planetraium.png", id: 0 },
+        { src: "https://i.postimg.cc/zvCBj8Sg/franky.png", id: 1 },
+        { src: "https://i.postimg.cc/3rzMz0xW/pexels-arina-krasnikova-6317441.jpg", id: 2 }
+    ]
     const windowElement: any = useRef(null);
     const minimizedWindowsElement: any = useRef(null)
-    const [projects, setProjects] = useState<Project[]>([{ src: "https://i.postimg.cc/MpCrVwHw/planetraium.png" }, { src: "https://i.postimg.cc/zvCBj8Sg/franky.png" }]);
+    const [projects, setProjects] = useState<Project[] | any>(initialProjects);
     const [minimizedWindow, setMinimizedWindows] = useState<Project[]>([])
     const handleWindowMove = (e: any) => {
         const windowsWindow: any = document.querySelector('.window');
@@ -22,33 +28,35 @@ const Projects = () => {
         windowsWindow.style.zIndex = 55;*/
     }
     const handleClose = (e: any) => {
+        // Copy projects arr to new array
+        const array = [...projects];
         // Fix the array to remove this window
+        array.splice(e.target.id, 1)
         setProjects([
-            ...projects.slice(0, e.target.id),
-            ...projects.slice(e.target.id + 1)
+            ...array
         ])
     }
-    const handleUnMinimize = (e: any) => {
-
+    const handleUnMinimize = (id:any, key:any) => {
+        const newItem: any = initialProjects[id];
+        //const newItem: any = minimizedWindow[id];
+        // First delete the minimized icon 
+        const placeholderArray = [...minimizedWindow]
+        placeholderArray.splice(key, 1)
+        setMinimizedWindows([
+            ...placeholderArray
+        ])
         // Then display the window again on the main screen
         setProjects([
             ...projects,
-            ...[minimizedWindow[e.target.id]]
+            newItem
         ])
-        // First delete the minimized icon 
-        setMinimizedWindows([
-            ...minimizedWindow.slice(0, e.target.id),
-            ...minimizedWindow.slice(e.target.id + 1)
-        ])
-
 
     }
     const handleMinimize = (e: any) => {
-
         handleClose(e);
         setMinimizedWindows([
-            ...[projects[e.target.id]],
-            ...minimizedWindow
+            ...minimizedWindow,
+            projects[e.target.id]
         ])
         // windowElement.current.style = "position: fixed; bottom:0; transform:scale(0.35); transition:0.7s; display:none"
         //const mirror = windowElement.cloneNode(false);
@@ -60,7 +68,7 @@ const Projects = () => {
 
     }
     const handleMaximize = () => {
-
+        // On maximize we will create the window on almost whole screen displaying project info
     }
     return (
         <div className='Projects'>
@@ -104,8 +112,9 @@ const Projects = () => {
             <ul className='minimizedWindows' ref={minimizedWindowsElement}>
                 {minimizedWindow.map((element: any, index: number) => {
                     return (
-                        <li className='minimizedWindow' onClick={(e) => handleUnMinimize(e)} id={index.toString()} key={index}>
+                        <li className='minimizedWindow' id={element.id} onClick={() => handleUnMinimize(element.id, index)} key={index}>
                             <img src="https://i.postimg.cc/rp57qS8t/981-9817075-vector-free-what-is-microsoft-explorer-internet-explorer.png" />
+                            <p>{element.id}</p>
                         </li>
                     )
                 })}
