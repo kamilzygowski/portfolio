eb86b9583904673a05298bd4d7fe1d976816dbd2
Kamil Żygowski <kzygowski7@gmail.com>
2022-06-09 02:53:24 +0200

    MobileGames layout planning

--- src/MobileVersion/MobileGames/MobileGames.tsx
+++ src/MobileVersion/MobileGames/MobileGames.tsx
@@ -1,8 +1,34 @@
 import React from 'react'
 import './MobileGames.scss'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Mousewheel } from 'swiper';
+import { url } from 'inspector';
+
 const MobileGames = () => {
+  const data = [
+    { img: "https://i.postimg.cc/vHrrQTby/deprev.png" },
+    { img: "https://i.postimg.cc/fT4hKNWr/milion-lasers.png" }
+  ]
   return (
-    <div className='MobileGames'>MobileGames</div>
+    <div className='MobileGames'>
+      <Swiper
+        direction={"horizontal"}
+        slidesPerView={1}
+        mousewheel={true}
+        observeParents={true}
+        observer={true}
+      >
+        {data.map((element: any, index: number) => {
+          const preparedStyle:any = (`url("${element.img}")`).toString()
+          return (
+            <SwiperSlide key={index} className="slide">
+              <div style={{backgroundImage: preparedStyle}} className='gamesBackground'>
+                </div>
+            </SwiperSlide>
+          )
+        })}
+      </Swiper>
+    </div>
   )
 }
 
