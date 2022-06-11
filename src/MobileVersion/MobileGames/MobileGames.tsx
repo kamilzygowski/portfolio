0c7402f42e6642714ff7851a81346d97c7060cef
Kamil Żygowski <kzygowski7@gmail.com>
2022-06-11 15:31:16 +0200

    cosmetics

--- src/MobileVersion/MobileGames/MobileGames.tsx
+++ src/MobileVersion/MobileGames/MobileGames.tsx
@@ -5,10 +5,17 @@ import { Pagination, Mousewheel } from 'swiper';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLocationArrow } from '@fortawesome/free-solid-svg-icons';
 
+interface MobileGame {
+  title: string;
+  img: string;
+  description: string;
+}
 const MobileGames = () => {
-  const data = [
-    { img: "https://i.postimg.cc/vHrrQTby/deprev.png" },
-    { img: "https://i.postimg.cc/fT4hKNWr/milion-lasers.png" }
+  const data: MobileGame[] = [
+    { title: "Mouse Sabre", img: "https://i.postimg.cc/63RQsW5L/FD.webp", description: 'Lorem ipsum dolor sit amet, cation ullamco laboris nisi uvoluptate velit esse cillum' },
+    { title: "Dimension Eye", img: "https://i.postimg.cc/vHrrQTby/deprev.png", description: 'Lorem ipsum dolor sit amet, cation ullamco laboris nisi uvoluptate velit esse cillum' },
+    { title: "Milion Lasers", img: "https://i.postimg.cc/fT4hKNWr/milion-lasers.png", description: 'Lorem ipsum dolor sit amet, cation ullamco laboris nisi uvoluptate velit esse cillum' },
+    { title: "Multi Arena", img: "https://i.postimg.cc/Bn49rBK5/multiarena.png", description: 'Lorem ipsum dolor sit amet, cation ullamco laboris nisi uvoluptate velit esse cillum' }
   ]
   return (
     <div className='MobileGames'>
@@ -28,11 +35,13 @@ const MobileGames = () => {
             </div>
           </div>
         </SwiperSlide>
-        {data.map((element: any, index: number) => {
+        {data.map((element: MobileGame, index: number) => {
           const preparedStyle: any = (`url("${element.img}")`).toString()
           return (
             <SwiperSlide key={index} className="slide">
               <div style={{ backgroundImage: preparedStyle }} className='gamesBackground'>
+                <h3 className='gameTitle'>{element.title}</h3>
+                <p className='gameDescription'>{element.description}</p>
               </div>
             </SwiperSlide>
           )
